3c77b3b48c5bc865d53b269e677d0e059b6e2ab2
alexa griffin <alexa.griffin42@gmail.com>
2019-01-04 22:07:43 -0500

    everything hurts and i want to die

--- core-redux_0.0.1/data-updates.lua
+++ core-redux_0.0.1/data-updates.lua
@@ -1,4 +1,18 @@
 if redux.core.settings.newScienceIcons then
   data.raw["tool"]["science-pack-1"].icon = "__core-redux__/graphics/icons/basic-science-pack.png"
   data.raw["tool"]["science-pack-2"].icon = "__core-redux__/graphics/icons/automation-science-pack.png"
+  data.raw["tool"]["science-pack-3"].icon = "__core-redux__/graphics/icons/automation-science-pack.png"
+  data.raw["tool"]["military-science-pack"].icon = "__core-redux__/graphics/icons/military-science-pack.png"
+  data.raw["tool"]["production-science-pack"].icon = "__core-redux__/graphics/icons/production-science-pack.png"
+  data.raw["tool"]["high-tech-science-pack"].icon = "__core-redux__/graphics/icons/avant-garde-science-pack.png"
+  data.raw["tool"]["space-science-pack"].icon = "__core-redux__/graphics/icons/space-science-pack.png"
+end
+
+if redux.core.settings.newOreIcons then
+  data.raw["item"]["iron-ore"].icon = "__core-redux__/graphics/icons/iron-ore.png"
+  data.raw["item"]["copper-ore"].icon = "__core-redux__/graphics/icons/copper-ore.png"
+  data.raw["item"]["stone"].icon = "__core-redux__/graphics/icons/stone.png"
+  data.raw["item"]["coal"].icon = "__core-redux__/graphics/icons/coal.png"
+  data.raw["item"]["coal"].dark_background_icon = "__core-redux__/graphics/icons/coal-dark-background.png"
+
 end
