0eb1499042ac67cb2537d104198e9c9550f26c82
alexa griffin <alexa.griffin42@gmail.com>
2019-01-05 16:46:25 -0500

    heck

--- logistic-redux_0.0.1/data-updates.lua
+++ logistic-redux_0.0.1/data-updates.lua
@@ -9,3 +9,17 @@ data.raw["recipe"]["science-pack-3"].ingredients = {
 
 -- item group updates
 data.raw["item"]["transport-belt"].subgroup = "basic-logistics"
+data.raw["item"]["underground-belt"].subgroup = "basic-logistics"
+data.raw["item"]["splitter"].subgroup = "basic-logistics"
+data.raw["item"]["burner-inserter"].subgroup = "basic-logistics"
+data.raw["item"]["inserter"].subgroup = "basic-logistics"
+data.raw["item"]["long-handed-inserter"].subgroup = "basic-logistics"
+
+data.raw["item"]["fast-transport-belt"].subgroup = "fast-logistics"
+data.raw["item"]["fast-underground-belt"].subgroup = "fast-logistics"
+data.raw["item"]["fast-splitter"].subgroup = "fast-logistics"
+data.raw["item"]["fast-inserter"].subgroup = "fast-logistics"
+
+data.raw["item"]["express-transport-belt"].subgroup = "express-logistics"
+data.raw["item"]["express-underground-belt"].subgroup = "express-logistics"
+data.raw["item"]["express-splitter"].subgroup = "express-logistics"
