f33979b1413c8a3b490c11131f42ec3ed737abb1
alexa griffin <alexa.griffin42@gmail.com>
2019-01-04 22:19:00 -0500

    heck

--- core-redux_0.0.1/data-updates.lua
+++ core-redux_0.0.1/data-updates.lua
@@ -1,7 +1,8 @@
+-- icon updates
 if redux.core.settings.newScienceIcons then
   data.raw["tool"]["science-pack-1"].icon = "__core-redux__/graphics/icons/basic-science-pack.png"
   data.raw["tool"]["science-pack-2"].icon = "__core-redux__/graphics/icons/automation-science-pack.png"
-  data.raw["tool"]["science-pack-3"].icon = "__core-redux__/graphics/icons/automation-science-pack.png"
+  data.raw["tool"]["science-pack-3"].icon = "__core-redux__/graphics/icons/chemical-science-pack.png"
   data.raw["tool"]["military-science-pack"].icon = "__core-redux__/graphics/icons/military-science-pack.png"
   data.raw["tool"]["production-science-pack"].icon = "__core-redux__/graphics/icons/production-science-pack.png"
   data.raw["tool"]["high-tech-science-pack"].icon = "__core-redux__/graphics/icons/avant-garde-science-pack.png"
@@ -14,5 +15,36 @@ if redux.core.settings.newOreIcons then
   data.raw["item"]["stone"].icon = "__core-redux__/graphics/icons/stone.png"
   data.raw["item"]["coal"].icon = "__core-redux__/graphics/icons/coal.png"
   data.raw["item"]["coal"].dark_background_icon = "__core-redux__/graphics/icons/coal-dark-background.png"
-
 end
+
+-- recipe updates
+data.raw["recipe"]["science-pack-3"].ingredients = {
+  { "advanced-circuit", 3 },
+  { "engine-unit", 2 },
+  { "solid-fuel", 1 }
+}
+
+data.raw["recipe"]["science-pack-3"].result_count = 2
+
+data.raw["recipe"]["military-science-pack"].ingredients = {
+  { "piercing-rounds-magazine", 1 },
+  { "grenade", 1 },
+  { "stone-wall", 1 },
+}
+
+data.raw["recipe"]["production-science-pack"].ingredients = {
+  { "rail", 30 },
+  { "electric-furnace", 1 },
+  { "productivity-module", 1 },
+}
+
+data.raw["recipe"]["production-science-pack"].result_count = 3
+
+
+data.raw["recipe"]["high-tech-science-pack"].ingredients = {
+  { "processing-unit", 2 },
+  { "flying-robot-frame", 1 },
+  { "low-density-structure", 3 },
+}
+
+data.raw["recipe"]["high-tech-science-pack"].result_count = 3
