ae47f09ec605176537f20c626a113ea00b280174
alexa griffin <alexa.griffin42@gmail.com>
2019-01-05 17:05:27 -0500

    heck

--- logistic-redux_0.0.1/data-updates.lua
+++ logistic-redux_0.0.1/data-updates.lua
@@ -5,7 +5,25 @@ data.raw["recipe"]["science-pack-3"].ingredients = {
   { "solid-fuel", 1 }
 }
 
+-- thanks to bob
+function redux.lib.beltIPS(ips)
+  return ips * 3/1280 --0.00234375
+end
+
 -- speed updates
+if redux.logistics.settings.highSpeed then
+  data.raw["transport-belt"]["transport-belt"].speed = redux.lib.beltIPS(15)
+  data.raw["underground-belt"]["underground-belt"].speed = redux.lib.beltIPS(15)
+  data.raw["splitter"]["splitter"].speed = redux.lib.beltIPS(15)
+
+  data.raw["transport-belt"]["fast-transport-belt"].speed = redux.lib.beltIPS(30)
+  data.raw["underground-belt"]["fast-underground-belt"].speed = redux.lib.beltIPS(30)
+  data.raw["splitter"]["fast-splitter"].speed = redux.lib.beltIPS(30)
+
+  data.raw["transport-belt"]["express-transport-belt"].speed = redux.lib.beltIPS(60)
+  data.raw["underground-belt"]["express-underground-belt"].speed = redux.lib.beltIPS(60)
+  data.raw["splitter"]["express-splitter"].speed = redux.lib.beltIPS(60)
+end
 
 -- item group updates
 data.raw["item"]["transport-belt"].subgroup = "basic-logistics"
---
+++ logistic-redux_0.0.1/settings.lua
@@ -0,0 +1,8 @@
+data:extend({
+  {
+    type = "bool-setting",
+    name = "redux-better-logistics",
+    setting_type = "startup",
+    default_value = true
+  },
+})
