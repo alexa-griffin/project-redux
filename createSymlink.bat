833c5a9cd3c5594b73c390fdb39bff1deecde3a3
alexa griffin <alexa.griffin42@gmail.com>
2019-02-02 23:12:09 -0500

    create symlink

---
+++ createSymlink.bat
@@ -0,0 +1,8 @@
+Mklink /D C:\Users\alexa\AppData\Roaming\Factorio\mods\core-redux_0.0.1 %CD%\core-redux_0.0.1
+Mklink /D C:\Users\alexa\AppData\Roaming\Factorio\mods\equipment-redux_0.0.1 %CD%\equipment-redux_0.0.1
+Mklink /D C:\Users\alexa\AppData\Roaming\Factorio\mods\logistic-redux_0.0.1 %CD%\logistic-redux_0.0.1
+Mklink /D C:\Users\alexa\AppData\Roaming\Factorio\mods\machine-redux_0.0.1 %CD%\machine-redux_0.0.1
+Mklink /D C:\Users\alexa\AppData\Roaming\Factorio\mods\module-redux_0.0.1 %CD%\module-redux_0.0.1
+
+
+pause
